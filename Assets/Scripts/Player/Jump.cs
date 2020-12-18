7e182c5119d187770e800719b18130379d168b11
Angelo Silva <myprojects.professional@gmail.com>
2020-12-17 22:33:01 -0300

    Adicionado controle do jogador sobre o personagem

---
+++ Assets/Scripts/Player/Jump.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    // ============================================
+    // VARIÁVEIS
+    // ============================================
+    public Animator animator;
+
+
+
+    // ============================================
+    // MÉTODOS
+    // ============================================
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Jump");
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Switch_Lanes.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch_Lanes : MonoBehaviour
+{
+    // ============================================
+    // VARIÁVEIS
+    // ============================================
+
+    public float speed;
+    public Rigidbody2D Rigidbody2D;
+
+    Vector2 offset;
+    // ============================================
+    // MÉTODOS
+    // ============================================
+
+    // ######### INICIALIZAÇÃO DE VARIÁVEIS
+
+    // ########## TRATA AS ENTRADAS DO TECLADO
+    void Update()
+    {
+        offset.y = Input.GetAxisRaw("Vertical");
+    }
+
+    // ########## TRATA OS MOVIMENTOS
+    void FixedUpdate()
+    {
+        // Troca de pista
+        Debug.Log("body.position: " + Rigidbody2D.position + " Offset: " + offset);
+
+
+        Rigidbody2D.MovePosition(Rigidbody2D.position + offset * speed * Time.fixedDeltaTime);
+        // Salto
+    }
+}
