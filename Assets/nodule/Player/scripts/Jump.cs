8f055adce6e5ba1771959af5bd0f147a581e19bc
bruno-reload <bruno-reload@live.com>
2020-12-18 15:20:36 -0300

    player moviment add

---
+++ Assets/nodule/Player/scripts/Jump.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    private Animator animator;
+
+   private void Start() {
+       animator = GetComponent<Animator>();
+   }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Jump");
+        }
+    }
+}
---
+++ Assets/nodule/Player/scripts/PlayerMoviment.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Comparers;
+
+public class PlayerMoviment : MonoBehaviour
+{
+    public float time;
+    public float distance;
+    private float direction;
+    private Vector3 startPosition;
+    private bool ableAnimation;
+
+
+    private void Start() {
+        startPosition = transform.position;
+    } 
+
+    private void Update()
+    {
+        direction = Input.GetAxisRaw("Vertical");
+        transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x,startPosition.y + direction, transform.position.z), time);
+       
+
+    }
+}
